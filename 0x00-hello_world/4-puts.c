882844b4cbe666f2b89ba9a10d341871abf5df22
Musa <musaqwabe@gmail.com>
2023-03-16 10:48:12 -0700

    2nd C program

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/*
+ * main - Entry point for the program
+ *
+ * this program prints the inputs on the terminal
+ * it calls on the other functions to perform it functionality 
+ *
+ * Return: Always return 0 on success
+ */
+
+int main (void)
+	{
+		puts("Programing is like building a multilingual puzzle.\n");
+		return (0);
+	}
