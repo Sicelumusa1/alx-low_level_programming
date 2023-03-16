f6cdc1c32cc9fde7852bab18427197cb8956fb7f
Musa <musaqwabe@gmail.com>
2023-03-16 10:58:52 -0700

    2nd code debugging

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,16 +1,14 @@
 #include <stdio.h>
 
-/*
+/**
  * main - Entry point for the program
- *
  * this program prints the inputs on the terminal
  * it calls on the other functions to perform it functionality 
- *
  * Return: Always return 0 on success
  */
 
-int main (void)
+int main(void)
 	{
-		puts("Programing is like building a multilingual puzzle.\n");
+		puts("Programing is like building a multilingual puzzle\n");
 		return (0);
 	}
