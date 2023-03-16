66873eac05d61cb83c88137bfd8b3fda30375622
Musa <musaqwabe@gmail.com>
2023-03-16 06:45:04 -0700

    Code debugging

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,7 +1,12 @@
 #include <stdio.h>
 
+/*
+ * main - This prints with proper grammar, but the outcome is a piece of art
+ * Return: This returns 0
+ */
+
 int main(void)
 	{
-		printf("with proper grammer, but the outcome is a piece of art,\n");
-		return(0);
+		printf("with proper grammar, but the outcome is a piece of art,\n");
+		return (0);
 	}
