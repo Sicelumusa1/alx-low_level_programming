8b9a80c4b52aa23ccc999558032b28669d48b369
Musa <musaqwabe@gmail.com>
2023-03-18 12:39:41 -0700

    putchar without using char

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main-Entry point for the program
+ * Return:Always 0 on success
+ */
+
+int main(void)
+	{
+		int NUM;
+
+		for (NUM = 0; NUM <= 9; NUM++)
+		{
+			putchar(NUM);
+		}
+		putchar(10);
+		return (0);
+	}
