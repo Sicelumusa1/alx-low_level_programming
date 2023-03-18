e298554a11b26536a54a5d5d7acce8fd1e21ea9d
Musa <musaqwabe@gmail.com>
2023-03-18 13:20:31 -0700

    not equalconditions

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -12,10 +12,11 @@
 int main(void)
 	{
 		int n;
+		int lastdigit;
 
 		srand(time (0));
 		n = rand() - RAND_MAX / 2;
-		int lastdigit = n% 10;
+		lastdigit = n % 10;
 
 		printf("Last digit of %d is" , n);
 		
@@ -32,6 +33,5 @@ int main(void)
 		else
 		{
 			printf("and is less than 6 and not 0\n");
-		}
-			return (0);
+		}	return (0);
 	}
