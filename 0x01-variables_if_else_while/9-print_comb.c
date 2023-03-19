5f8bcc537f44cd07b8c271db032e09cbefc915ba
Musa <musaqwabe@gmail.com>
2023-03-19 07:22:51 -0700

    ASCII and putchar debugging

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -9,11 +9,14 @@ int main(void)
 	{
 		int i;
 
-		for (i = '0';i <= '9';i++)
+		for (i = '0'; i <= '9' ; i++)
 		{
 			putchar(i);
+		if (i != '9')
+		{
 			putchar(44);
 			putchar(32);
+		}
 		}
 		putchar(10);
 		return (0);
