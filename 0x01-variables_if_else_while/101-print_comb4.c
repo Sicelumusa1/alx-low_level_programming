1c7ceaa4508d81a322cb50b7f42f79a8789aef63
Musa <musaqwabe@gmail.com>
2023-03-20 09:10:15 -0700

    combo numbers

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+
+/**
+ * main-Entry point for the program
+ * Return:Always 0 on success
+ */
+
+int main(void)
+	{
+		int a, b, c;
+
+		for (a = 48 ; a <= 55; a++)
+		{
+		for (b = 49; b <= 56; b++)
+		{
+		for (c = 50; c <= 57; c++)
+		{
+			if (c > b > a)
+			{
+				putchar(a);
+				putchar(b);
+				putchar(c);
+			}
+		if (a != 55 || b != 56 || c != 57)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+		}
+		}
+		}
+		putchar('\n');
+		return (0);
+	}
+
