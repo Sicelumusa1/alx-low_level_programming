8ff0e029b87849df1d52b1ecf9d4b0f4719526f1
Musa <musaqwabe@gmail.com>
2023-03-21 18:32:30 -0700

    _putchar

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,5 +1,5 @@
 #ifndef MAIN_H
-
+#define MAIN_H
 int _putchar(char c);
 void print_alphabet(void);
 void print_alphabet_x10(void);
@@ -13,5 +13,4 @@ void times_table(void);
 int add(int a, int b);
 void print_to_98(int n);
 
-#define MAIN_H
 #endif
