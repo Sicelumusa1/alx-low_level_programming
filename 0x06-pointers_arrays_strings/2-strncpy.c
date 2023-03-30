552a606ec9f8d41f4ce6a04b58651fc1c162193f
Musa <musaqwabe@gmail.com>
2023-03-30 10:38:03 -0700

    debuged strncpy and strcmp codes

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -12,7 +12,7 @@ char *_strncpy(char *dest, char *src, int n)
 {
 	char *result = dest;
 
-	while (n != '\0')
+	while (n > 0)
 	{
 
 		if ((*result = *src) != 0)
--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -9,24 +9,15 @@
 
 int _strcmp(char *s1, char *s2)
 {
-	int i = 0; 
-	int areEqual;
+	int i = 0;
 
-	while (s1[i] == s2[i])
-	{
-		i++;
-	}
 	while (s1[i] != '\0' && s2[i] != '\0')
 	{
+		if (s1[i] == s2[i])
+		{
+			return (s1[i] - s2[i]);
+		}
 		i++;
 	}
-	if (s1[i] == '\0' && s2[i] != '\0')
-	{
-		areEqual = 1;
-	}
-	else
-	{
-		areEqual = -1;
-	}
-	return (areEqual);
+	return (s1[i] - s2[i]);
 }
