63ab7b7a00cd6291a5a65e1c7df19f5c485f8aca
Musa <musaqwabe@gmail.com>
2023-03-30 13:17:13 -0700

    debugged strcmp

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -10,14 +10,17 @@
 int _strcmp(char *s1, char *s2)
 {
 	int i = 0;
+	int strcmp;
 
 	while (s1[i] != '\0' && s2[i] != '\0')
 	{
-		if (s1[i] == s2[i])
+		strcmp = s1[i] - s2[i];
+
+		if (strcmp != 0)
 		{
-			return (s1[i] - s2[i]);
+			return (strcmp);
 		}
 		i++;
 	}
-	return (s1[i] - s2[i]);
+	return (0);
 }
