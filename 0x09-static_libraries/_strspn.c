1bcbce9289b755c1867acf664d1f3230eaf9c5c0
Musa <musaqwabe@gmail.com>
2023-04-07 14:11:37 -0700

    static lib

--- 0x09-static_libraries/_strspn.c
+++ 0x09-static_libraries/_strspn.c
@@ -1,15 +1,1 @@
 
-
-unsigned int _strspn(char *s, char *accept)
-{
-        char *p;
-
-        for (p = s; *p != '\0'; p++)
-        {
-                if (!strchr(accept, *p))
-                {
-                        break;
-                }
-        }
-        return (p - s);
-}
