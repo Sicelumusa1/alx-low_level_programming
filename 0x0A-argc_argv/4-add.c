c60c5972bc4e061f4b9455ea5b6587a95af33b1c
Musa <musaqwabe@gmail.com>
2023-04-10 03:14:59 -0700

    argc,argv

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -12,13 +12,7 @@ int main(int argc, char *argv[])
 {
 	int i = 1, num = 0;
 	int result = 0;
-	
-	/*if (*argv[1] < '0' || *argv[i] > '9')
-	{
-		printf("error\n");
-		return (1);
-	}*/	
-
+		
 	if (argc < 2)
 	{
 		printf("0\n");
