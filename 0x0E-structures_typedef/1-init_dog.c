37f8ba45c9d5cf6972cbbdca6d393ad771cdb2f2
Musa <musaqwabe@gmail.com>
2023-04-17 19:59:52 -0700

    structures typedef

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -12,13 +12,6 @@
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
 
-	struct dog
-	{
-		char *name;
-		char *owner;
-		float *age;
-	};
-
 	(*d).name = name;
 	(*d).owner = owner;
 	(*d).age = age;
--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -8,11 +8,14 @@
  *  @age: dogs' age
  */
 
-struct dog
+typedef struct dog
 {
 	char *name;
 	char *owner;
 	float age;
-};
+}dog_t;
+
+void print_dog(struct dog *d);
+void init_dog(struct dog *d, char *name, float age, char *owner);
 
 #endif /* STRUCT_DOG_H */
