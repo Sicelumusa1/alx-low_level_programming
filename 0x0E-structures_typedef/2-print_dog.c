ffc9f313b3bb57eaeed43b8fe58cb59665717c8f
Musa <musaqwabe@gmail.com>
2023-04-17 20:43:11 -0700

    2-print_dog debug

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -16,17 +16,15 @@ void print_dog(struct dog *d)
 		return;
 	}
 
-	if(d->name == NULL)
+	if (d->name == NULL)
 	{
 	       printf("nil");
 	}
-
-	age = d->age;
 
 	if (d->owner == NULL)
 	{
 		printf("nil");
 	}
 
-	printf("Name: %s\nAge: %f\nOwner: %s\n", name, age, owner);
+	printf("Name: %s\nAge: %f\nOwner: %s\n", d->name, d->age, d->owner);
 }
--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -17,5 +17,7 @@ typedef struct dog
 
 void print_dog(struct dog *d);
 void init_dog(struct dog *d, char *name, float age, char *owner);
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
 
 #endif /* STRUCT_DOG_H */
