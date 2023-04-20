1d67751896cecfa076aa6688a1358ab3ba8136b4
Musa <musaqwabe@gmail.com>
2023-04-20 03:33:32 -0700

    debbuged print_strings

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -20,7 +20,7 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 	for (i = 1; i <= n; i++)
 	{
 		printf("%d", va_arg(lst, int));
-		if (separator != NULL)
+		if (separator != NULL && i != n)
 		{
 			printf("%s", separator);
 		}
