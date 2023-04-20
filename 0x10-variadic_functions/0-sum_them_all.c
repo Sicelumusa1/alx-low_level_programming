a5cc2c1d58cc7fb47104f330e2ddd763f3660a15
Musa <musaqwabe@gmail.com>
2023-04-20 12:05:56 -0700

    print all

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -14,13 +14,14 @@ int sum_them_all(const unsigned int n, ...)
 	va_list list;
 
 	va_start(list, n);
+	
+	if (n == 0)
+	{
+		return (0);
+	}
 
 	for (i = 1; i <= n; i++)
 	{
-		if (n == 0)
-		{
-			return (0);
-		}
 		sum += va_arg(list, int);
 	}
 	va_end(list);
---
+++ 0x10-variadic_functions/3-print_all.c
@@ -0,0 +1,71 @@
+#include "variadic_functions.h"
+#include <stdio.h>
+#include <stdarg.h>
+
+/**
+ * print_all - function that prints anything
+ * @format: list of types of arguments passed
+ */
+
+void print_all(const char *const format, ...)
+{
+	va_list lst;
+	const char *ptr;
+	int i;
+	char *s;
+	char c;
+	float f;
+	
+	if (format == NULL)
+		return;
+
+	va_start(lst, format);
+	while ((ptr = format))
+	{
+		while (*ptr != '\0')
+		{
+			switch (*ptr)
+			{
+				case 'i':
+				i = va_arg(lst, int);
+				printf("%d", i);
+				break;
+
+				case 'c':
+				c = va_arg(lst, int);
+				printf("%c", c);
+				break;
+
+				case 'f':
+				f = va_arg(lst, double);
+				printf("%f", f);
+				break;
+
+				case 's':
+				s = va_arg(lst, char *);
+				if (s == NULL)
+				{
+					printf("(nil)");
+				}
+				printf("%s", s);
+				break;
+
+				default:
+
+				break;
+				}
+				if (*(ptr + 1) != '\0' && (*ptr == 'i' ||
+							*ptr == 'c' ||
+							*ptr == 'f' ||
+							*ptr == 's'))
+				{
+					printf(", ");
+				}
+				ptr++;
+		}
+
+	}
+
+	printf("\n");
+	va_end(lst);
+}
