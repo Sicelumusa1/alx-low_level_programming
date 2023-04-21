a4fe5f9e9e8011f45699a0b73c0b3f8738afd52c
Musa <musaqwabe@gmail.com>
2023-04-21 03:05:45 -0700

    debug array_iterator

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -12,7 +12,7 @@ void array_iterator(int *array, size_t size, void(*action)(int))
 {
 	size_t i = 0;
 
-	if (action == NULL || i >= size)
+	if (action == NULL || array == NULL)
 		return;
 
 	for (i = 0; i < size ; i++)
