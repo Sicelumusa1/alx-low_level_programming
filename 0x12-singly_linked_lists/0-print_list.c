2ffa62019ef101ef08942c71bc276cb38d51ef86
Musa <musaqwabe@gmail.com>
2023-04-28 06:12:39 -0700

    added 0 print list

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,30 @@
+#include "lists.h"
+
+/**
+ * print_list - prints all elements of a linked list
+ * @h: pointer containg  the  address of the first node of the list
+ * Return: number of nodes
+ */
+
+size_t print_list(const list_t *h)
+{
+	size_t count = 0;
+	const list_t *ptr = h;
+
+	while (ptr != NULL)
+	{
+		if (ptr->str == NULL)
+		{
+			printf("[0] (nil)\n");
+		}
+		else
+		{
+			printf("%s\n", ptr->str);
+		}
+		ptr = ptr->next;
+		count++;
+	}
+
+	return (count);
+}
+
